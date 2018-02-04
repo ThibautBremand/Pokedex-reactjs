4dc7000efeb088b03aac1aaaeeb7aa8b9698c89e
Thibaut BREMAND <thibaut.bremand@gmail.com>
2018-02-04 21:28:50 +0100

    moves tutors UI reworked

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -142,22 +142,21 @@ class Pokemon extends Component {
                                             this.props.movesTutors.map((gen, l) => {
                                                 let targetDiv = `collapseMoveTutor${l}`;
                                                 let idTargetDiv = `#${targetDiv}`;
-                                                console.log(targetDiv);
-                                                return (
-                                                    <div className="" key={l}>
 
-                                                        <button className="btn btn-secondary" type="button" data-toggle="collapse" data-target={idTargetDiv}  aria-expanded="false" aria-controls={idTargetDiv}>
-                                                            {gen[0].gen}
+                                                return (
+                                                    <div key={l}>
+
+                                                        <button className="btn btn-secondary btnMovesTutors" type="button" data-toggle="collapse" data-target={idTargetDiv}  aria-expanded="false" aria-controls={idTargetDiv}>
+                                                            <span>{gen[0].gen}</span>
                                                         </button>
 
                                                         <div className="collapse" id={targetDiv}>
-                                                            <div className="card card-block">
+                                                            <div className="card card-block cardMovesTutors">
                                                             {
                                                                 gen.map((move, i) => {
                                                                     return (
                                                                         <div className="row" key={i}>
                                                                             <div className="col-md-1"></div>
-                                                                            <div className="movesLevel col">{move.gen}</div>
                                                                             <div className="movesName col">{move.name}</div>
                                                                         </div>
                                                                     )
